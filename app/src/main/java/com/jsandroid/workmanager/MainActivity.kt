8dd125b79696abca7e34ed689f4f34874ffcb56c
Jeongsik Mun <farfs.dev@gmail.com>
2018-12-22 23:32:53 +0900

    updates

--- app/src/main/java/com/jsandroid/workmanager/MainActivity.kt
+++ app/src/main/java/com/jsandroid/workmanager/MainActivity.kt
@@ -2,12 +2,19 @@ package com.jsandroid.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val WORK_TAG = "WORK_TAG"
+    }
+
 
     private val simpleRequest =
         OneTimeWorkRequest.Builder(SimpleWorker::class.java).build()
@@ -17,8 +24,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startSimpleWorker.setOnClickListener {
+        startSimpleWorkerBtn.setOnClickListener {
             workManager.beginWith(simpleRequest).enqueue()
+            val status = workManager.getWorkInfoByIdLiveData(simpleRequest.id)
+            status.observe(this, Observer<WorkInfo> { info ->
+                val workFinished = info?.state?.isFinished == true
+                workStatusText.text = when {
+                    workFinished -> "work status: finished"
+                    else -> "work status: running"
+                }
+            })
         }
     }
 }
--- app/src/main/java/com/jsandroid/workmanager/SimpleWorker.kt
+++ app/src/main/java/com/jsandroid/workmanager/SimpleWorker.kt
@@ -1,6 +1,7 @@
 package com.jsandroid.workmanager
 
 import android.content.Context
+import android.os.SystemClock
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
@@ -11,7 +12,8 @@ class SimpleWorker(context : Context, params : WorkerParameters)
     override fun doWork(): Result {
         val number = 10
         val result = number * number
+        SystemClock.sleep(5000)
         Log.d("SimpleWorker", "SimpleWorker finished: $result")
-        return Result.SUCCESS
+        return Result.success()
     }
 }
