cf4abe16e1e48f73f04fd59c536d78afe9b01355
Jeongsik Mun <farfs.dev@gmail.com>
2018-12-23 09:31:22 +0900

    updats

--- app/src/main/java/com/jsandroid/workmanager/MainActivity.kt
+++ app/src/main/java/com/jsandroid/workmanager/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jsandroid.workmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.work.Data
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkInfo
@@ -16,24 +17,38 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private val simpleRequest =
-        OneTimeWorkRequest.Builder(SimpleWorker::class.java).build()
-    private val workManager = WorkManager.getInstance()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         startSimpleWorkerBtn.setOnClickListener {
+            val inputData = Data.Builder()
+                .putInt(SimpleWorker.EXTRA_NUMBER, 5)
+                .build()
+
+            val simpleRequest = OneTimeWorkRequest.Builder(SimpleWorker::class.java)
+                .setInputData(inputData)
+                .build()
+
+            val workManager = WorkManager.getInstance()
             workManager.beginWith(simpleRequest).enqueue()
+
             val status = workManager.getWorkInfoByIdLiveData(simpleRequest.id)
             status.observe(this, Observer<WorkInfo> { info ->
                 val workFinished = info?.state?.isFinished == true
+                val result = info?.outputData?.getInt(SimpleWorker.EXTRA_RESULT, 0)
                 workStatusText.text = when {
-                    workFinished -> "work status: finished"
+                    workFinished -> "work status: finished, result: ${result}"
                     else -> "work status: running"
                 }
             })
         }
+
+
+
+
     }
 }
--- app/src/main/java/com/jsandroid/workmanager/SimpleWorker.kt
+++ app/src/main/java/com/jsandroid/workmanager/SimpleWorker.kt
@@ -3,17 +3,28 @@ package com.jsandroid.workmanager
 import android.content.Context
 import android.os.SystemClock
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 class SimpleWorker(context : Context, params : WorkerParameters)
         : Worker(context, params) {
 
+    companion object {
+        const val EXTRA_NUMBER = "EXTRA_NUMBER"
+        const val EXTRA_RESULT = "EXTRA_RESULT"
+    }
+
     override fun doWork(): Result {
-        val number = 10
+        val number = inputData.getInt(EXTRA_NUMBER, 0)
         val result = number * number
         SystemClock.sleep(5000)
         Log.d("SimpleWorker", "SimpleWorker finished: $result")
-        return Result.success()
+
+        val outputData = Data.Builder()
+            .putInt(SimpleWorker.EXTRA_RESULT, result)
+            .build()
+
+        return Result.success(outputData)
     }
 }
