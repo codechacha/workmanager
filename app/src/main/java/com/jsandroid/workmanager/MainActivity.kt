b6b4be080e3291ea9f23f19b6e544b5983bc5f83
Jeongsik Mun <farfs.dev@gmail.com>
2018-12-23 09:47:11 +0900

    updates

--- app/src/main/java/com/jsandroid/workmanager/MainActivity.kt
+++ app/src/main/java/com/jsandroid/workmanager/MainActivity.kt
@@ -18,11 +18,12 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val workManager = WorkManager.getInstance()
 
         startSimpleWorkerBtn.setOnClickListener {
             val inputData = Data.Builder()
@@ -31,20 +32,30 @@ class MainActivity : AppCompatActivity() {
 
             val simpleRequest = OneTimeWorkRequest.Builder(SimpleWorker::class.java)
                 .setInputData(inputData)
+                .addTag(WORK_TAG)
                 .build()
 
-            val workManager = WorkManager.getInstance()
             workManager.beginWith(simpleRequest).enqueue()
-
             val status = workManager.getWorkInfoByIdLiveData(simpleRequest.id)
             status.observe(this, Observer<WorkInfo> { info ->
                 val workFinished = info?.state?.isFinished == true
                 val result = info?.outputData?.getInt(SimpleWorker.EXTRA_RESULT, 0)
                 workStatusText.text = when {
-                    workFinished -> "work status: finished, result: ${result}"
-                    else -> "work status: running"
+                    workFinished -> {
+                        "work status: finished, state=${info?.state} result: ${result}"
+                    }
+                    else -> {
+                        "work status: running"
+                    }
                 }
             })
+        }
+
+        cancelSimpleWorkerBtn.setOnClickListener {
+            val workManager = WorkManager.getInstance()
+            workManager.cancelAllWorkByTag(WORK_TAG)
+            // workManager.cancelWorkById(simpleRequest.id)
+            // workManager.cancelAllWork()
         }
 
 
